b9fe65232b42c5509a75ffe0939d494a5565cd04
patiencewanjiru <patiencewanjiru123@gmail.com>
2023-05-22 13:51:40 +0300

    Completed assignment

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,27 @@
+class Book
+    attr_accessor :title, :author, :page_count, :genre
+    
+    def initialize(title)
+        @title = title
+    end
 
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end
+end
+
+# Example usage and output
+book = Book.new("And Then There Were None")
+puts book.title
+
+book.author = "Agatha Christie"
+puts book.author
+
+book.page_count = "300 pages"
+puts book.page_count
+
+book.genre = "Mystery"
+puts book.genre
+
+book.turn_page
 
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,40 @@
 # Make your shoe class here!
+class Shoe
+    attr_accessor :brand, :color, :size, :material, :condition
+    
+    def initialize(brand)
+        @brand = brand
+        @condition = "new"
+    end
+
+    def cobble
+        puts "Your shoe is as good as new!"
+        @condition = "new"
+    end
+end
+
+# Example usage of the Shoe class
+shoe = Shoe.new("Nike")
+puts shoe.brand
+
+puts shoe.color
+
+shoe.color = "Black"
+puts shoe.color
+
+puts shoe.size
+
+shoe.size = 10
+puts shoe.size
+
+puts shoe.material
+
+shoe.material = "Leather"
+
+puts shoe.material
+
+puts shoe.condition
+
+shoe.cobble
+
+puts shoe.condition
